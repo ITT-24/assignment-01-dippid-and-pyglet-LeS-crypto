4115385347e0a2b1e70e79125b3721b49c4d5ee8
Leonie Schrod <leonie.schrod@t-online.de>
2024-04-20 18:32:43 +0200

    🚧 working sender with dummy data

--- dippid-sender/DIPPID-sender.py
+++ dippid-sender/DIPPID-sender.py
@@ -1,6 +1,7 @@
 import socket
 import time 
 import numpy as np
+import json
 
 IP = '127.0.0.1'
 PORT = 5700
@@ -12,17 +13,73 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # Implement plausible behaviour for simulated sensors
         # e.g. sine functions with diff frequencies for each axis of acc
 
-# class Capabilities:
-#     def __init__(self, name:str, value:dict):
-#         pass
+# ----- PARENT ----- #
+class Capabilities:
+    def __init__(self, name:str):
+        self.data = self._simulate_data()
+
+    def _simulate_data(self, data=None):
+        """Abstract class to override"""
+        return data
+
+# ----- child-classes ----- #
+class Accelerometer(Capabilities):
+    def __init__(self, name="accelerometer"):
+        self.name=name
+        super().__init__(name=name)
+
+    def _simulate_data(self) -> str:
+        data = {
+                "x": self._simulate_axis(axis="x"),
+                "y": self._simulate_axis(axis="y"),
+                "z": self._simulate_axis(axis="z"),
+                }
+        # data = json.dumps(data) # works
+        return super()._simulate_data(data)
+    
+    def _simulate_axis(self, axis:str) -> str:
+        # TODO
+        axis_data = "0"
+        return axis_data
+
+
+class Button(Capabilities):
+    def __init__(self, name='button_1'):
+        self.name = name
+        super().__init__(name=name)
+    
+    def _simulate_data(self, data=None):
+        # data = {self.name: "clicked"}
+        data = "clicked"
+        # data = json.dumps(data)
+        return super()._simulate_data(data)
+
+
+def create_simulated_data() -> str:
+    acc = Accelerometer() 
+    btn_1 = Button()
+    
+    data = { 
+        acc.name: acc.data, 
+        btn_1.name: btn_1.data
+    } 
+
+    return json.dumps(data)
+
+# ----- SEND-LOOP ----- #
+
 counter = 0
+
 while True:
     
-    # "data" = 'data' demo_reciever.py/register_callback
-    message = '{"data" : ' + str(counter) + '}'
-    print(message)
+    sim_data = create_simulated_data()
+    print("sim_data", sim_data)
+    
+    # message = '{"accelerometer": {"x": "0"} }' # works
+    # message = '{"accelerometer": {"x": "0", "y": "0", "z": "0"} }' # works
+    # print("nnot_sim", message)
 
-    sock.sendto(message.encode(), (IP, PORT))
+    sock.sendto(sim_data.encode(), (IP, PORT))
 
     counter += 1
     time.sleep(1)
--- dippid-sender/demo_receiver.py
+++ dippid-sender/demo_receiver.py
@@ -1,11 +1,25 @@
 from DIPPID import SensorUDP
+from time import sleep
 # INFO: see demo_hearbeat.py
 
 # use UPD (via WiFi) for communication
 PORT = 5700
 sensor = SensorUDP(PORT)
 
-def handle_data(data):
-    print(data)
+while(True):
+    print("caps", sensor.get_capabilities())
 
-sensor.register_callback('data', handle_data)
+    if(sensor.has_capability('accelerometer')):
+        print(sensor.get_value('accelerometer'))
+        acc_x = float(sensor.get_value('accelerometer')['x'])
+        print(acc_x)
+
+    sleep(1)
+
+# def handle_data(data):
+#     print(data)
+#     # print("data", sensor.get_value('accelerometer'))
+#     # print(float(sensor.get_value('accelerometer')['x']))
+#     # print(data)
+
+# sensor.register_callback('data', handle_data)
