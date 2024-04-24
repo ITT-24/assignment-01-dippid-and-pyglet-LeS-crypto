76254a1caaf7596fb7009bb5d1cfae141cc12e6e
Leonie Schrod <leonie.schrod@t-online.de>
2024-04-24 14:53:03 +0200

    add: collision + score

--- 2d-game/catch.py
+++ 2d-game/catch.py
@@ -8,10 +8,18 @@ window = pyglet.window.Window(WIDTH, HEIGHT)
 # batch = pyglet.graphics.Batch()
 
 # ----- GAME ELEMENTS ----- #
-player = shapes.Rectangle(150, 50, 100, 10, (255, 0, 0))
+PLAYER_WIDTH = 100
+PLAYER_HEIGHT = 10
+PLAYER_Y = 50
+player = shapes.Rectangle(150, PLAYER_Y, 
+                          PLAYER_WIDTH, PLAYER_HEIGHT, 
+                          (255, 0, 0))
+
+score = pyglet.text.Label(text="Score: 0", x=10, y=380)
 
 class Target():
     targets = []
+    count = 0
 
     def draw_targets():
         for target in Target.targets:
@@ -46,15 +54,51 @@ class Target():
             Target.update_single(target)
 
     def update_single(target):
+       # make 'em fall!
        target.y -= 10
-        # make 'em fall!
+        # TODO: speed up when time progresses
 
     def check_bounds(target):
+        # collision works, but a little laggy
+        # only works for center of circle => TODO
+        
+        player_max_x = player.x + PLAYER_WIDTH
+        player_max_y = player.y + PLAYER_HEIGHT
+
+        # only check collision, if circle at bottom of screen
+        if target.y <= player_max_y:
+            # make 'em disappear if out of y_bottom_bounds
+            min_y = 0 - target.radius
+            if target.y < min_y:
+                Target.targets.remove(target)
+                # print("rm target")
+
+             # make 'em disappear if collision with player
+            if target.y >= PLAYER_Y: # and  target.y <= player_max_y:
+                if target.x >= player.x and target.x <= player_max_x:
+                    Target.targets.remove(target)
+                    
+                    # update Score
+                    Target.count += 1
+                    score.text=f"Score: {Target.count}"
+
+    def check_bounds_old(target):
         # make 'em disappear if out of y_bottom_bounds
         min_y = 0 - target.radius
         if target.y < min_y:
             Target.targets.remove(target)
+        
+        # get bounds of player
+        player_max_y = player.y + PLAYER_HEIGHT
+        player_max_x = player.x + PLAYER_WIDTH
+        
         # make 'em disappear if collision with player
+        if target.y >= PLAYER_Y and target.y <= player_max_y:
+            if target.x >= player.x and target.x <= player_max_x:
+                Target.targets.remove(target)
+                print("collision")
+        # collision works, but a little laggy
+        # only works for center of circle => TODO
 
 
 # ----- GAME LOOPS ----- # 
@@ -62,13 +106,14 @@ class Target():
 def on_draw():
     window.clear()
 
+    score.draw()
     player.draw()
     Target.draw_targets()
 
 
 
 clock.schedule_interval(Target.create_target, 1)
-# TODO: increase create_intervall from 1 to 0.01 the longer the game runs
+# TODO: gradually increase create_target intervall from 1 to 0.01 the longer the game runs
 clock.schedule_interval(Target.update_targets, 0.1)
 
 
@@ -76,6 +121,11 @@ clock.schedule_interval(Target.update_targets, 0.1)
 def on_key_press(symbol, modifiers):
     if symbol == pyglet.window.key.Q:
         window.close()
+    # addition keybinds for optional keyboard usage/debug
+    elif symbol == pyglet.window.key.LEFT:
+        player.x -= 20
+    elif symbol == pyglet.window.key.RIGHT:
+        player.x += 20
 
 # ----- RUN GAME ----- #
 
